2fbe22ff54b3d0231ecd998ff12406dd70eb7825
Takahiro Sugaya <takskg@gmail.com>
2019-08-29 02:44:26 +0900

    add:バーテックスシェーダーとフラグメントシェーダーの追加とコンパイルバッチを追加

---
+++ hello-vulkan/resources/shader/compile.bat
@@ -0,0 +1,7 @@
+rem コンパイル
+glslangValidator.exe ezshader.vert -V -S vert -o ezshader.vert.spv
+glslangValidator.exe ezshader.frag -V -S vert -o ezshader.frag.spv
+
+rem リソースを出力先にコピー
+copy /Y ezshader.vert.spv ..\..\..\resources\shader\ezshader.vert.spv
+copy /Y ezshader.frag.spv ..\..\..\resources\shader\ezshader.frag.spv
