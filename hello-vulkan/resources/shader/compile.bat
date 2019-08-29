8e89161c930f54691265b0630e1ded693081b3de
Takahiro Sugaya <takskg@gmail.com>
2019-08-30 00:52:08 +0900

    fix:フラグメントシェーダーがバーテックスシェーダーとしてコンパイルされてたのを修正

--- hello-vulkan/resources/shader/compile.bat
+++ hello-vulkan/resources/shader/compile.bat
@@ -1,6 +1,6 @@
 rem コンパイル
 glslangValidator.exe ezshader.vert -V -S vert -o ezshader.vert.spv
-glslangValidator.exe ezshader.frag -V -S vert -o ezshader.frag.spv
+glslangValidator.exe ezshader.frag -V -S frag -o ezshader.frag.spv
 
 rem リソースを出力先にコピー
 copy /Y ezshader.vert.spv ..\..\..\resources\shader\ezshader.vert.spv
