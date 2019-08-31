f9985c21a36f13c678b1fcdbd16dc6216d546d0f
Takahiro Sugaya <takskg@gmail.com>
2019-09-01 00:22:38 +0900

    add:Cube描画用シェーダーを追加

--- hello-vulkan/resources/shader/compile.bat
+++ hello-vulkan/resources/shader/compile.bat
@@ -1,7 +1,11 @@
 rem コンパイル
 glslangValidator.exe ezshader.vert -V -S vert -o ezshader.vert.spv
+glslangValidator.exe texshader.vert -V -S vert -o texshader.vert.spv
 glslangValidator.exe ezshader.frag -V -S frag -o ezshader.frag.spv
+glslangValidator.exe texshader.frag -V -S frag -o texshader.frag.spv
 
 rem リソースを出力先にコピー
 copy /Y ezshader.vert.spv ..\..\..\resources\shader\ezshader.vert.spv
+copy /Y texshader.vert.spv ..\..\..\resources\shader\texshader.vert.spv
 copy /Y ezshader.frag.spv ..\..\..\resources\shader\ezshader.frag.spv
+copy /Y texshader.frag.spv ..\..\..\resources\shader\texshader.frag.spv
